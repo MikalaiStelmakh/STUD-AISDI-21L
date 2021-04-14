1973d8f12936751634d495b3359308eb849b4275
Mikalai Stelmakh <mikalai.stelmakh@gmail.com>
2021-04-14 20:07:28 +0200

    Add benchmarks

--- lab3/test_trees.py
+++ lab3/test_trees.py
@@ -1,4 +1,19 @@
-from Trees import AVLTree
+from Trees import AVLTree, BST
+import pytest
+import random
+import sys
+import copy
+
+sys.setrecursionlimit(10000)
+
+NUMBERS = random.sample(range(1, 30001), 10000)
+SIZES = [1000, 2000, 4000, 6000, 8000, 10000]
+
+FILLED_TREES = []
+AVL = AVLTree(NUMBERS)
+BST = BST()
+BST.insert_list(NUMBERS)
+FILLED_TREES = [BST, AVL]
 
 
 def test_init_1():
@@ -19,19 +34,19 @@ def test_init_3():
 def test_insert_find_1():
     avltree = AVLTree([1, 2, 3, 4])
     avltree.insert(5)
-    assert avltree.findNode(5).val == 5
+    assert avltree.search(5).val == 5
 
 
 def test_insert_find_2():
     avltree = AVLTree([1, 2, 3, 4])
     avltree.insert(2.5)
-    assert avltree.findNode(2.5).val == 2.5
+    assert avltree.search(2.5).val == 2.5
 
 
 def test_delete_1():
     avltree = AVLTree([1, 2, 3, 4])
     avltree.delete(2)
-    assert avltree.findNode(2) is None
+    assert avltree.search(2) is None
 
 
 def test_delete_2():
@@ -58,3 +73,39 @@ def test_height():
     n = 16
     avltree = AVLTree([i for i in range(2 ** n)])
     assert avltree.getHeight(avltree.root) <= n + 1
+
+
+@pytest.mark.parametrize('size', SIZES)
+def test_insertTimeAVL(size, benchmark):
+    benchmark.extra_info["function"] = "insertTime"
+    benchmark.extra_info["tree"] = "AVL"
+    benchmark.extra_info["size"] = size
+    benchmark(AVLTree, NUMBERS[:size])
+
+
+# @pytest.mark.parametrize('size', SIZES)
+# def test_insertTimeBST(size, benchmark):
+#     benchmark.extra_info["function"] = "insertTime"
+#     benchmark.extra_info["tree"] = "BST"
+#     benchmark.extra_info["size"] = size
+#     bst = BST()
+#     benchmark(bst.insert_list, NUMBERS[:size])
+
+
+@pytest.mark.parametrize("tree", FILLED_TREES)
+@pytest.mark.parametrize("size", SIZES)
+def test_searchTime(tree, size, benchmark):
+    benchmark.extra_info["function"] = "searchTime"
+    benchmark.extra_info["tree"] = tree
+    benchmark.extra_info["size"] = size
+    benchmark(tree.search_list, NUMBERS[:size])
+
+
+@pytest.mark.parametrize("tree", FILLED_TREES)
+@pytest.mark.parametrize("size", SIZES)
+def test_deleteTime(tree, size, benchmark):
+    benchmark.extra_info["function"] = "deleteTime"
+    benchmark.extra_info["tree"] = tree
+    benchmark.extra_info["size"] = size
+    new_tree = copy.deepcopy(tree)
+    benchmark(new_tree.delete_list, NUMBERS[:size])
