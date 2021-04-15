960c8267c64207ef19e9bf827f81e331013e254a
Mikalai Stelmakh <mikalai.stelmakh@gmail.com>
2021-04-15 10:52:45 +0200

    Add createCharts function

--- lab3/benchmarkCharts.py
+++ lab3/benchmarkCharts.py
@@ -23,10 +23,26 @@ def read_from_json(path):
     return info
 
 
-def createCharts():
-    pass
+def createCharts(info, figs, axes):
+    for function, fig, ax in zip(info, figs, axes):
+        for tree in info[function]:
+            sizes = []
+            times = []
+            for size in info[function][tree]:
+                sizes.append(size)
+                times.append(info[function][tree][size] * 10**3)
+            ax.plot(sizes, times, label=tree)
+        ax.set_title(function)
+        ax.set_xlabel('Number of elements')
+        ax.set_ylabel('Time [ms]')
+        ax.legend()
+        fig.savefig('lab3/charts/' + function)
 
 
 if __name__ == '__main__':
-    print(read_from_json('lab3/.benchmarks/data.json'))
-
+    plt.style.use('seaborn')
+    info = read_from_json('lab3/.benchmarks/data.json')
+    fig1, ax1 = plt.subplots()
+    fig2, ax2 = plt.subplots()
+    fig3, ax3 = plt.subplots()
+    createCharts(info, [fig1, fig2, fig3], [ax1, ax2, ax3])
