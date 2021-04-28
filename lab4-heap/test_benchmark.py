75a035b9e28aec8682fe692889a2a98b471479d1
Mikalai Stelmakh <mikalai.stelmakh@gmail.com>
2021-04-28 20:29:31 +0200

    Add benchmarks

--- lab4-heap/test_benchmark.py
+++ lab4-heap/test_benchmark.py
@@ -10,7 +10,8 @@ SIZES = [1000, 2000, 4000, 6000, 8000, 10000]
 DIMENSIONS = [2, 3, 4]
 FILLED_HEAPS = [Heap(dimension=2), Heap(dimension=3), Heap(dimension=4)]
 for heap in FILLED_HEAPS:
-    heap.push(NUMBERS)
+    for number in NUMBERS:
+        heap.push(number)
 ids = [2, 3, 4]
 
 
@@ -21,7 +22,8 @@ def test_push_n_elements_time(dimension, size, benchmark):
     to {dimension}-ary heap."""
     def func():
         heap = Heap(dimension=dimension)
-        heap.push(NUMBERS[:size])
+        for number in NUMBERS[:size]:
+            heap.push(number)
     benchmark.extra_info["function"] = "push"
     benchmark.extra_info["dimension"] = dimension
     benchmark.extra_info["size"] = size
@@ -40,19 +42,5 @@ def test_pop_n_times_time(heap, dimension, size, benchmark):
     benchmark.extra_info["function"] = "pop_n_times"
     benchmark.extra_info["dimension"] = dimension
     benchmark.extra_info["size"] = size
-    benchmark.pedantic(func, rounds=BENCHMARK_ROUNDS)
+    benchmark.pedantic(func)
 
-
-@pytest.mark.parametrize("heap, dimension", zip(FILLED_HEAPS, DIMENSIONS), ids=ids)
-@pytest.mark.parametrize("size", SIZES)
-def test_pop_n_times_method_time(heap, dimension, size, benchmark):
-    """Benchmark Heap class method 'pop_n_times' that pops the topmost element {size} times from {dimension}-ary heap.
-    Much more efficient when removing a large number of elements.
-    Time is inversely propotional to the time of using pop function {size} times"""
-    def func():
-        heapCopy = copy.deepcopy(heap)
-        heapCopy.pop_n_times(size)
-    benchmark.extra_info["function"] = "pop_n_times_method"
-    benchmark.extra_info["dimension"] = dimension
-    benchmark.extra_info["size"] = size
-    benchmark.pedantic(func, rounds=BENCHMARK_ROUNDS)
