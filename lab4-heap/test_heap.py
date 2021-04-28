ca7344767bf9b7503c0380baa141567bad21c484
Mikalai Stelmakh <mikalai.stelmakh@gmail.com>
2021-04-28 20:29:14 +0200

    Add tests

--- lab4-heap/test_heap.py
+++ lab4-heap/test_heap.py
@@ -1,56 +1,51 @@
-from heap import Node, Heap
+from heap import Heap
 import pytest
-
 
 DIMENSIONS = [2, 3, 4]
 
 
 @pytest.mark.parametrize("dimension", DIMENSIONS)
-def test_initialize_empty_heap(dimension):
-    heap = Heap(dimension=dimension)
-    assert heap.root is None
-
-
-@pytest.mark.parametrize("dimension", DIMENSIONS)
 def test_initialize_heap(dimension):
-    heap = Heap(dimension=dimension, value=10)
-    assert heap.root.val == 10
+    heap = Heap(dimension=dimension)
+    assert heap.get_raw_data() == []
 
 
 @pytest.mark.parametrize("dimension", DIMENSIONS)
-def test_push_to_empty_heap(dimension):
+def test_push_single_value_to_empty_heap(dimension):
     heap = Heap(dimension=dimension)
-    heap.push([10, 20, 30])
-    assert heap.root.val == 10
-    assert heap.root.firstChild.val == 20
-    assert heap.root.secondChild.val == 30
+    heap.push(10)
+    assert heap.get_raw_data() == [10]
 
 
-def test_push_to_two_dimension_heap():
-    heap = Heap(dimension=2, value=30)
-    heap.push([20, 40, 50, 4])
-    assert heap.root.val == 30
-    assert heap.root.firstChild.val == 20
-    assert heap.root.secondChild.val == 40
-    assert heap.root.firstChild.firstChild.val == 50
-    assert heap.root.firstChild.secondChild.val == 4
+@pytest.mark.parametrize("dimension", DIMENSIONS)
+def test_push_to_heap(dimension):
+    heap = Heap(dimension=dimension)
+    heap.push(20)
+    heap.push(10)
+    heap.push(30)
+    heap.push(19)
+    heap.push(22)
+    assert heap.get_raw_data() == [10, 19, 20, 22, 30]
 
 
-def test_push_to_three_dimension_heap():
-    heap = Heap(dimension=3, value=30)
-    heap.push([20, 40, 50, 4])
-    assert heap.root.val == 30
-    assert heap.root.firstChild.val == 20
-    assert heap.root.secondChild.val == 40
-    assert heap.root.thirdChild.val == 50
-    assert heap.root.firstChild.firstChild.val == 4
+@pytest.mark.parametrize("dimension", DIMENSIONS)
+def test_pop_from_heap(dimension):
+    heap = Heap(dimension=dimension)
+    heap.push(20)
+    heap.push(10)
+    heap.push(30)
+    heap.push(19)
+    heap.push(22)
+    assert heap.pop() == 10
+    assert heap.get_raw_data() == [19, 20, 30, 22]
 
 
-def test_push_to_four_dimension_heap():
-    heap = Heap(dimension=4, value=30)
-    heap.push([20, 40, 50, 4])
-    assert heap.root.val == 30
-    assert heap.root.firstChild.val == 20
-    assert heap.root.secondChild.val == 40
-    assert heap.root.thirdChild.val == 50
-    assert heap.root.fourthChild.val == 4
+@pytest.mark.parametrize("dimension", DIMENSIONS)
+def test_get_heap_length(dimension):
+    heap = Heap(dimension=dimension)
+    heap.push(20)
+    heap.push(10)
+    heap.push(30)
+    heap.push(19)
+    heap.push(22)
+    assert len(heap) == 5
