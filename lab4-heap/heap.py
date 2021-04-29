ec58a3e3d38a850d5883332b29a1f661f715f71e
Mikalai Stelmakh <mikalai.stelmakh@gmail.com>
2021-04-29 11:49:27 +0200

    Add _print function and comments

--- lab4-heap/heap.py
+++ lab4-heap/heap.py
@@ -1,5 +1,4 @@
 from abstract_heap import AbstractHeap
-import random
 
 
 class Heap(AbstractHeap):
@@ -9,21 +8,29 @@ class Heap(AbstractHeap):
         self.dimension = dimension
         self._data = []
 
-    def shift_down(self, startpos, pos):
+    def shift_down(self, startpos: int, pos: int) -> None:
+        """ If new element is less that its parent, swap them
+            and do the same for all subsequent elements."""
         newitem = self._data[pos]
         # Follow the path to the root, moving parents down until finding
         # a place newitem fits.
         while pos > startpos:
             parentpos = (pos - 1) // 2
             parent = self._data[parentpos]
+            # If (inserted element < parent) - swap them
+            # and check all elements after parent's index.
             if newitem < parent:
                 self._data[pos] = parent
                 pos = parentpos
                 continue
+            # Break if inserted element >= parent.
             break
+        # Insert new element to list.
         self._data[pos] = newitem
 
-    def shift_up(self, pos):
+    def shift_up(self, pos: int) -> None:
+        """ If element at {pos} is greater that element at child pos, swap them
+            and do the same for all subsequent elements."""
         endpos = len(self._data)
         startpos = pos
         newitem = self._data[pos]
@@ -43,16 +50,16 @@ class Heap(AbstractHeap):
         self._data[pos] = newitem
         self.shift_down(startpos, pos)
 
-    def peek(self):
+    def peek(self) -> C:
         """Get the topmost element without changing the heap."""
         return self._data[0]
 
-    def push(self, item):
+    def push(self, item: C) -> None:
         """Add an element to the heap."""
         self._data.append(item)
         self.shift_down(0, len(self._data)-1)
 
-    def pop(self):
+    def pop(self) -> C:
         """Remove the topmost element from the heap and return it."""
         deleted = self._data[0]
         del self._data[0]
@@ -60,47 +67,34 @@ class Heap(AbstractHeap):
             self.shift_up(0)
         return deleted
 
-    def get_raw_data(self):
+    def get_raw_data(self) -> List[C]:
         """Get the underlying data storage."""
         return self._data
 
-    def pprint(self, root: int = 0, depth: int = 0) -> str:
-        str_: str = ""
-
-        leftmost_child = root*self.dimension + 1
-        children = list(range(
-            leftmost_child,
-            min(len(self), leftmost_child + self.dimension)
-        ))
-
-        left_children = children[:self.dimension//2]
-        right_children = children[self.dimension//2:]
-
-        for child in reversed(right_children):
-            str_ += self.pprint(child, depth + 4)
-        str_ += " "*depth + repr(self._data[root]) + "\n"
-        for child in reversed(left_children):
-            str_ += self.pprint(child, depth + 4)
-
-        return str_
-
-    # def _print(self, index=0, level=0):
-    #     children = [self._data[index*self.dimension + indx] for indx in range(1, self.dimension+1)
-    #                 if index*self.dimension + indx < len(self._data)]
-    #     for child in reversed(children):
-    #         new_index = children.index(child) + 1
-    #         self._print(index+new_index, level+1)
-    #         print("\t"*(level+1) + "->", child)
+    def _print(self, index=0, level=0):
+        """Prints out a tree."""
+        children = [self._data[index*self.dimension + indx] for indx
+                    in range(1, self.dimension+1)
+                    if index*self.dimension + indx < len(self._data)]
+        if children:
+            left_children = [children[i] for i in range(self.dimension // 2) if i < len(children)]
+            right_children = children[self.dimension//2:]
+            for child in reversed(right_children):
+                new_index = (children.index(child)+1) + index*self.dimension
+                self._print(new_index, level+1)
+        print("\t"*level + "->", self._data[index])
+        if children:
+            for child in reversed(left_children):
+                new_index = (children.index(child)+1) + index*self.dimension
+                self._print(new_index, level+1)
 
 
 if __name__ == '__main__':
-    heap = Heap(2)
-
-    NUMBERS = random.sample(range(1, 30001), 10000)
-    for number in NUMBERS:
-        heap.push(number)
-    for _ in range(10000):
-        heap.pop()
-
-
-
+    heap = Heap(4)
+    heap.push(20)
+    heap.push(10)
+    heap.push(30)
+    heap.push(19)
+    heap.push(22)
+    print(heap.get_raw_data())
+    heap._print()
