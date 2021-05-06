56b4a387fa1fbb1f16d9b4f26f86ac3b1c51000a
Mikalai Stelmakh <mikalai.stelmakh@gmail.com>
2021-05-06 16:36:06 +0200

    Add function declarations

---
+++ lab5-pattern_search/pattern_search.py
@@ -0,0 +1,34 @@
+def find_n(substring: str, text: str):
+    """
+    Parameters:
+        substring: substring to be found
+        text: text to be searchedP
+    Returns:
+        List of positions in ascending ordeer of the beginnings
+        of ``substring`` in ``text``.
+    """
+    pass
+
+
+def find_kmp(substring: str, text: str):
+    """
+    Parameters:
+        substring: substring to be found
+        text: text to be searchedP
+    Returns:
+        List of positions in ascending ordeer of the beginnings
+        of ``substring`` in ``text``.
+    """
+    pass
+
+
+def find_kr(substring: str, text: str):
+    """
+    Parameters:
+        substring: substring to be found
+        text: text to be searchedP
+    Returns:
+        List of positions in ascending ordeer of the beginnings
+        of ``substring`` in ``text``.
+    """
+    pass
