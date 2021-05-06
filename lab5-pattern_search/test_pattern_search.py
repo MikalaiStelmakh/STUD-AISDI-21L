22ab22ce7834b0248dda109e8231c98f0d4bc662
Mikalai Stelmakh <mikalai.stelmakh@gmail.com>
2021-05-06 16:36:56 +0200

    Add tests

---
+++ lab5-pattern_search/test_pattern_search.py
@@ -0,0 +1,42 @@
+from pattern_search import find_n, find_kmp, find_kr
+import random
+import pytest
+
+FUNCTIONS = [find_n, find_kmp, find_kr]
+TEXT = """  Nam porttitor, elit quis rhoncus molestie, lorem massa
+            sollicitudin leo, ac auctor orci magna at est. Donec in
+            ipsum congue, lobortis purus non, euismod lectus.
+            Vestibulum rhoncus gravida auctor."""
+
+
+@pytest.mark.parametrize("function", FUNCTIONS)
+def test_empty_text(function):
+    assert function(substring="aBc", text="") == []
+
+
+@pytest.mark.parametrize("function", FUNCTIONS)
+def test_empty_substring(function):
+    assert function(substring="", text="abcd") == [0, 1, 2, 3]
+
+
+@pytest.mark.parametrize("function", FUNCTIONS)
+def test_substring_equals_text(function):
+    assert function(substring=TEXT, text=TEXT) == [0]
+
+
+@pytest.mark.parametrize("function", FUNCTIONS)
+def test_substring_longer_than_text(function):
+    substring = TEXT + "a"
+    assert function(substring=substring, text=TEXT) == []
+
+
+@pytest.mark.parametrize("function", FUNCTIONS)
+def test_text_dont_contains_substring(function):
+    assert function(substring="hello", text="world") == []
+
+
+def test_random_letters():
+    text = "".join(random.choices(["a", "b"], k=100))
+    substring = "".join(random.choices(["a", "b"], k=10))
+    assert find_n(substring, text) == find_kmp(substring, text)
+    assert find_n(substring, text) == find_kr(substring, text)
