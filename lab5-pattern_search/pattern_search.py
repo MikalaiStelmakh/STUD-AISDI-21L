a69e40dc95321bde88f6acc4db6ced64dffd0527
Roman Ishchuk <01165722@pw.edu.pl>
2021-05-09 16:24:25 +0000

    Update pattern_search.py

--- lab5-pattern_search/pattern_search.py
+++ lab5-pattern_search/pattern_search.py
@@ -7,7 +7,17 @@ def find_n(substring: str, text: str):
         List of positions in ascending ordeer of the beginnings
         of ``substring`` in ``text``.
     """
-    pass
+    if not substring:
+        return list(range(len(text)))
+    naive_position = []
+    if substring and text:
+        for n in range(0, len(text) - len(substring) + 1):
+            m = 0
+            while m < len(substring) and text[n + m] == substring[m]:
+                m += 1
+                if m == len(substring):
+                    naive_position.append(n)
+    return naive_position
 
 
 def find_kmp(substring: str, text: str):
