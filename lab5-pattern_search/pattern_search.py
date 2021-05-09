79d4ba0c56583a89d843d1d58c045ce4708743e8
Roman Ishchuk <01165722@pw.edu.pl>
2021-05-09 19:18:03 +0000

    Update pattern_search.py

--- lab5-pattern_search/pattern_search.py
+++ lab5-pattern_search/pattern_search.py
@@ -29,7 +29,37 @@ def find_kmp(substring: str, text: str):
         List of positions in ascending ordeer of the beginnings
         of ``substring`` in ``text``.
     """
-    pass
+    if not substring:
+        return list(range(len(text)))
+    kmp_position = []
+    if substring and text:
+        prefix = [0 for i in range(len(substring))]
+        i = 0
+        j = 1
+        while j < len(substring):
+            if substring[i] == substring[j]:
+                prefix[j] = i + 1
+                i += 1
+                j += 1
+            elif i != 0:
+                i = prefix[i - 1]
+            else:
+                j += 1
+        print(prefix)
+        n = 0
+        m = 0
+        while n < len(text):
+            if text[n] == substring[m]:
+                n += 1
+                m += 1
+            elif m != 0:
+                m = prefix[m - 1]
+            else:
+                n += 1
+            if m == len(substring):
+                kmp_position.append(n - m)
+                m = prefix[m - 1]
+    return kmp_position
 
 
 def find_kr(substring: str, text: str):
