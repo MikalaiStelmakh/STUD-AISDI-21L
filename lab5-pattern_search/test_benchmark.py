44987ece3ffb758cf05ad192b7849ae147c13a76
Mikalai Stelmakh <mikalai.stelmakh@gmail.com>
2021-05-11 18:37:55 +0200

    Change sizes and fix function names

--- lab5-pattern_search/test_benchmark.py
+++ lab5-pattern_search/test_benchmark.py
@@ -8,20 +8,21 @@ def read_from_file(file_path) -> str:
     return data
 
 
+BENCHMARK_ROUNDS = 5
 TEXT = read_from_file("lab5-pattern_search/pan-tadeusz.txt")
 TEXT_LIST = TEXT.split()
 
-SIZES = [100, 200, 300, 400, 500, 600, 700, 800, 1000]
+SIZES = [100, 300, 500, 700, 1000]
 FUNCTIONS = [find_n, find_kmp, find_kr]
 ids = ["Naive", "Knuth Morris Pratt", "Karp Rabin"]
 
 
 @pytest.mark.parametrize("size", SIZES)
-@pytest.mark.parametrize("function", FUNCTIONS, ids=ids)
-def test_pattern_search(size, function, benchmark):
+@pytest.mark.parametrize("function, function_name", zip(FUNCTIONS, ids), ids=ids)
+def test_pattern_search(size, function, function_name, benchmark):
     def func():
         for pattern in TEXT_LIST[:size]:
             function(substring=pattern, text=TEXT)
-    benchmark.extra_info["function"] = function
+    benchmark.extra_info["function"] = function_name
     benchmark.extra_info["size"] = size
-    benchmark.pedantic(func, rounds=20)
+    benchmark.pedantic(func, rounds=BENCHMARK_ROUNDS)
