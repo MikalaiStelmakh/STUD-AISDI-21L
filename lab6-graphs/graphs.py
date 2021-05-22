4076909cd865092cb89369369b25754fb9ae289c
Roman Ishchuk <01165722@pw.edu.pl>
2021-05-22 16:13:19 +0000

    Update graphs.py

--- lab6-graphs/graphs.py
+++ lab6-graphs/graphs.py
@@ -41,8 +41,55 @@ class Graph():
             raise ValueError("The list should contain exactly two zeros")
         return positions
 
+    def minDistance(self, dist, queue):
+        minimum = float("Inf")
+        min_index = -1
+        for i in dist.keys():
+            if dist[i] < minimum and i in queue:
+                minimum = dist[i]
+                min_index = i
+        return min_index
+
+    def posible(self, pos, x=0, y=0):
+        posNext = (pos[0] + x, pos[1] + y)
+        if 0 <= posNext[0] < len(self.array) and 0 <= posNext[1] < len(self.array[0]):
+            return posNext
+        return None
+
     def dijkstra(self):
-        pass
+        dist = dict()
+        parent = dict()
+        queue = []
+        for row in range(len(self.array)):
+            for column in range(len(self.array[row])):
+                dist[(row, column)] = float("Inf")
+                parent[(row, column)] = tuple()
+                queue.append((row, column))
+
+        start, finish = self.find_zero_positions()
+
+        dist[start] = 0
+
+        while finish in queue:
+            vertexFrom = self.minDistance(dist, queue)
+            queue.remove(vertexFrom)
+            xOffset, yOffset = [1, -1, 0, 0], [0, 0, 1, -1]
+            for dir in range(4):
+                vertexTo = self.posible(vertexFrom,
+                                        x=xOffset[dir], y=yOffset[dir])
+                if vertexTo:
+                    i, j = vertexTo
+                    if dist[vertexFrom] + self.array[i][j] < dist[vertexTo]:
+                        dist[vertexTo] = dist[vertexFrom] + self.array[i][j]
+                        parent[vertexTo] = vertexFrom
+        step = finish
+        path = []
+        while step != start:
+            path.append(step)
+            step = parent[step]
+        path.append(step)
+        path.reverse()
+        return path
 
 
 if __name__ == "__main__":
