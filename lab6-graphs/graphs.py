335477fd3c47ce94d490dec7b9a0c1270c73537f
Mikalai Stelmakh <mikalai.stelmakh@gmail.com>
2021-05-23 17:52:50 +0200

    Add PathPrint function, remake read from txt function to class method

--- lab6-graphs/graphs.py
+++ lab6-graphs/graphs.py
@@ -1,20 +1,7 @@
 import argparse
 from typing import List, Tuple
 
-
-def read_from_txt(path) -> List[int]:
-    """
-    Parameters:
-        path: path to file to be readed.
-    Returns:
-        Two dimensional list of numbers from file.
-    """
-    result = []
-    with open(path) as f:
-        for line in f:
-            line = line.rstrip()
-            result.append([int(number) for number in line])
-    return result
+Point = Tuple[int, int]
 
 
 class Graph():
@@ -22,9 +9,8 @@ class Graph():
         if not all(len(array[0]) == len(row) for row in array):
             raise ValueError("The rows must be the same length")
         self.array = array
-        self.source, self.destination = self.find_zero_positions()
 
-    def find_zero_positions(self) -> List[Tuple[int]]:
+    def find_source_dest_positions(self, value) -> List[Point]:
         """
         Parameters:
             None
@@ -35,14 +21,78 @@ class Graph():
         """
         positions = []
         for row_index, row in enumerate(self.array):
-            if 0 in row:
-                positions.append((row_index, row.index(0)))
+            if value in row:
+                positions.append((row_index, row.index(value)))
         if len(positions) != 2:
-            raise ValueError("The list should contain exactly two zeros")
+            raise ValueError("There must be exactly one source and one destination value.\n")
         return positions
 
-    def dijkstra(self):
-        pass
+    def minDistance(self, dist, queue):
+        minimum = float("Inf")
+        min_index = -1
+        for i in dist.keys():
+            if dist[i] < minimum and i in queue:
+                minimum = dist[i]
+                min_index = i
+        return min_index
+
+    def posible(self, pos, x=0, y=0):
+        posNext = (pos[0] + x, pos[1] + y)
+        if 0 <= posNext[0] < len(self.array) and 0 <= posNext[1] < len(self.array[0]):
+            return posNext
+        return None
+
+    def dijkstra(self, value: int) -> List[Point]:
+        dist = dict()
+        parent = dict()
+        queue = []
+        for row in range(len(self.array)):
+            for column in range(len(self.array[row])):
+                dist[(row, column)] = float("Inf")
+                parent[(row, column)] = tuple()
+                queue.append((row, column))
+        start, finish = self.find_source_dest_positions(value)
+        dist[start] = 0
+        while finish in queue:
+            vertexFrom = self.minDistance(dist, queue)
+            queue.remove(vertexFrom)
+            xOffset, yOffset = [1, -1, 0, 0], [0, 0, 1, -1]
+            for dir in range(4):
+                vertexTo = self.posible(vertexFrom,
+                                        x=xOffset[dir], y=yOffset[dir])
+                if vertexTo:
+                    i, j = vertexTo
+                    if dist[vertexFrom] + self.array[i][j] < dist[vertexTo]:
+                        dist[vertexTo] = dist[vertexFrom] + self.array[i][j]
+                        parent[vertexTo] = vertexFrom
+        step = finish
+        path = []
+        while step != start:
+            path.append(step)
+            step = parent[step]
+        path.append(step)
+        path.reverse()
+        return path
+
+    def find_shortest_path(self, value: int) -> str:
+        path = self.dijkstra(value)
+        result = ""
+        sorted_path = [sorted([x[1] for x in path if x[0] == row_num])
+                       for row_num in range(len(self.array))]
+        for row_num, row in enumerate(sorted_path):
+            for column_num in range(len(self.array[0])):
+                result += str(self.array[row_num][column_num]) if column_num in row else " "
+            result += "\n"
+        return result[:-1]
+
+    @classmethod
+    def make_from_txt(_class, path):
+        result = []
+        with open(path) as f:
+            for line in f:
+                line = line.rstrip()
+                result.append([int(number) for number in line])
+        return _class(result)
 
 
 if __name__ == "__main__":
@@ -50,6 +100,6 @@ if __name__ == "__main__":
     parser.add_argument("file")
     args = parser.parse_args()
 
-    array = read_from_txt(args.file)
-    graph = Graph(array)
-
+    graph = Graph.make_from_txt(args.file)
+    path = graph.find_shortest_path(0)
+    print(path)
